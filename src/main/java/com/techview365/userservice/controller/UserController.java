8805171d0ef14fe48a718a71bd40a2c7e24d5d4b
Manish <moneyyiiss@gmail.com>
2024-06-01 18:21:39 +0530

    User service

---
+++ src/main/java/com/techview365/userservice/controller/UserController.java
@@ -0,0 +1,4 @@
+package com.techview365.userservice.controller;
+
+public class UserController {
+}
---
+++ src/test/java/com/techview365/userservice/UserserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techview365.userservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
